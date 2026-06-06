6e122e9669c62398dff734324b2a0246ba17bbfd
SRamirez313 <156979123+SRamirez313@users.noreply.github.com>
2026-06-05 18:21:44 -0600

    Add files via upload
    semana3

---
+++ semana3-web/src/main/java/com/curso/semana3_web/Semana3WebApplication.java
@@ -0,0 +1,13 @@
+package com.curso.semana3_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana3WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana3WebApplication.class, args);
+	}
+
+}
